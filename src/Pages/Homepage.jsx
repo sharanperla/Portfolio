57596e93a4e77933452f0c09e7b430e0ff77551a
sharan kumar <sharanperla1234@gmail.com>
2023-09-27 00:06:55 +0530

    host issue solved

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../Components/Navbar';
 import Banner from '../Components/Banner';
 import About from '../Components/about/About';
-import Projects from '../Components/projects';
+import Projects from '../Components/Projects';
 import Footer from '../Components/Footer';
 function Homepage() {
   return (
