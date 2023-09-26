0b86335f665a75bb847cb977617e18c6dfc3c984
sharan kumar <sharanperla1234@gmail.com>
2023-09-26 17:41:58 +0530

    checking

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 
 function Projects() {
-  return (
-    <div className='h-full my-3'>
-    <div className='mt-2 w-[90%] bg-[#ebebeb] p-5 rounded-xl mx-auto text-center md:text-[2rem] font-bold' >Projects</div>
-    <div className="cards w-[90%] mx-auto mt-2 flex flex-wrap gap-3 justify-center">
-     <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
-lhk
-     </div>
-     <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
-lhk
-     </div>
-     <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
-lhk
-     </div>
-     <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
-lhk
-     </div>
-     <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
-lhk
-     </div>
-    </div>
-    </div>
-  )
+     return (
+          <div className='h-full my-3'>
+               <div className='mt-2 w-[90%] bg-[#ebebeb] p-5 rounded-xl mx-auto text-center md:text-[2rem] font-bold' >Projects</div>
+               <div className="cards w-[90%] mx-auto mt-2 flex flex-wrap gap-3 justify-center">
+                    <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
+                         lhk
+                    </div>
+                    <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
+                         lhk
+                    </div>
+                    <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
+                         lhk
+                    </div>
+                    <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
+                         lhk
+                    </div>
+                    <div className="card bg-[#ebebeb] h-96 w-[100%] md:w-[49%] rounded-xl">
+                         lhk
+                    </div>
+               </div>
+          </div>
+     )
 }
 
 export default Projects;
--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -7,7 +7,7 @@ function About() {
     return (
 
         <div className="wrapper relative bg-primary mt-44 ">
-            <div class="custom-shape-divider-top-1693470071">
+            <div className="custom-shape-divider-top-1693470071">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className='w-full h-full'>
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="fill-main"></path>
                 </svg>
@@ -23,7 +23,7 @@ function About() {
                         <FontAwesomeIcon icon={faJs} className='item hover:text-blue-600 hover:scale-125' />
                         <FontAwesomeIcon icon={faReact} className='item hover:text-blue-600 hover:scale-125' />
                         <FontAwesomeIcon icon={faWind} className='item hover:text-blue-600 hover:scale-125' />
-                        <FontAwesomeIcon icon={faBootstrap} cclassName='item hover:text-blue-600 hover:scale-125' />
+                        <FontAwesomeIcon icon={faBootstrap} className='item hover:text-blue-600 hover:scale-125' />
                         <FontAwesomeIcon icon={faGithub} className='item hover:text-blue-600 hover:scale-125' />
                         <FontAwesomeIcon icon={faFigma} className='item hover:text-blue-600 hover:scale-125' />
                         <FontAwesomeIcon icon={faPhp} className='item hover:text-blue-600 hover:scale-125' />
